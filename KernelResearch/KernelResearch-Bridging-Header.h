9a5e6412ad612068925dd12d79ae4fa9279b1d14
kevinisgoated24-spec <kevinisgoated24@gmail.com>
2026-09-17 15:47:32 -0700

    son

--- KernelResearch/KernelResearch-Bridging-Header.h
+++ KernelResearch/KernelResearch-Bridging-Header.h
@@ -4,3 +4,35 @@
 #import "bad_query.h"
 #import "iokit_fuzz.h"
 #import "metal_trampoline.h"
+
+// vm_region_64 is callable on iOS but Apple marks all *_COUNT macros
+// and info structs as "unavailable: structure not supported" in the SDK.
+// Define a compatible struct directly so we bypass those restrictions.
+#ifndef VM_REGION_COMPAT_DEFINED
+#define VM_REGION_COMPAT_DEFINED
+
+#define VM_REGION_EXTENDED_INFO_COMPAT 13  // flavor value from XNU source
+
+struct vm_region_extended_info_compat {
+    int            protection;
+    unsigned int   user_tag;
+    unsigned int   pages_resident;
+    unsigned int   pages_shared_now_private;
+    unsigned int   pages_swapped_out;
+    unsigned int   pages_dirtied;
+    unsigned int   ref_count;
+    unsigned short shadow_depth;
+    unsigned char  external_pager;
+    unsigned char  share_mode;
+    int            is_submap;
+    int            behavior;
+    unsigned int   object_id;
+    unsigned short user_wired_count;
+    unsigned short _pad;
+};
+typedef struct vm_region_extended_info_compat vm_region_extended_info_compat_t;
+
+#define VM_REGION_EXTENDED_INFO_COMPAT_COUNT \
+    ((mach_msg_type_number_t)(sizeof(vm_region_extended_info_compat_t) / sizeof(int)))
+
+#endif
--- KernelResearch/metal_fuzz.swift
+++ KernelResearch/metal_fuzz.swift
@@ -1827,14 +1827,14 @@ func runVMRegionScan(log: FuzzLog, completion: @escaping () -> Void) {
 
         while totalRegions < 1500 && kernelPtrs < 150 {
             var size:    vm_size_t = 0
-            var info     = vm_region_extended_info_data_t()
-            var count    = mach_msg_type_number_t(VM_REGION_EXTENDED_INFO_COUNT)
+            var info     = vm_region_extended_info_compat_t()
+            var count    = mach_msg_type_number_t(VM_REGION_EXTENDED_INFO_COMPAT_COUNT)
             var objName: mach_port_t = 0
 
             let kr: kern_return_t = withUnsafeMutablePointer(to: &info) { ip in
                 ip.withMemoryRebound(to: Int32.self, capacity: Int(count)) { rp in
                     vm_region_64(mach_task_self_, &addr, &size,
-                                 VM_REGION_EXTENDED_INFO, rp, &count, &objName)
+                                 VM_REGION_EXTENDED_INFO_COMPAT, rp, &count, &objName)
                 }
             }
             guard kr == KERN_SUCCESS else { break }
